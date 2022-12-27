6fd2159113d697b79dfaeab00628c5d14282e6d9
JosephDedicated <infotostalin@gmail.com>
2022-12-28 00:08:57 +0530

    debug

--- app/src/main/java/com/instanceit/alhadafpos/Fragments/ServiceOrder/ServiceOrderFragment.java
+++ app/src/main/java/com/instanceit/alhadafpos/Fragments/ServiceOrder/ServiceOrderFragment.java
@@ -2289,7 +2289,7 @@ public class ServiceOrderFragment extends Fragment implements AdapterCallback {
                         total_price_withqty += (cartArrayList.get(i).getSummaryDetails().get(j).getQty()
                                 * (cartArrayList.get(i).getSummaryDetails().get(j).getFinalprice()));
                         total_item_discount_all+=total_item_discount;
-                        total_taxable_amount_withqty += cartArrayList.get(i).getSummaryDetails().get(j).getTaxable()-total_item_discount;
+                        total_taxable_amount_withqty += cartArrayList.get(i).getSummaryDetails().get(j).getPrice()-total_item_discount;
                         total_tax_amount_withqty += cartArrayList.get(i).getSummaryDetails().get(j).getIgsttaxamt();
                         cartArrayList.get(i).getSummaryDetails().get(j).setFinalprice(cartArrayList.get(i).getSummaryDetails().get(j).getFinalprice()-total_item_discount);
                         total_payable_amount_withqty += cartArrayList.get(i).getSummaryDetails().get(j).getFinalprice();
